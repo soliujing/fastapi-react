0188d10a5f78093f4b79c90f8cc4212df6611f77
soliujing <soliujing@gmail.com>
2025-05-22 17:18:10 +0800

    comprehensive log for uvicorn

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -33,8 +33,8 @@ class Settings(BaseSettings):
     )
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    # 60 minutes = 1 hour
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
